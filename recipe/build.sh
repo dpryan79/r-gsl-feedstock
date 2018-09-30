98155122869d1dd67b8c9dfb1f95ea051290ccd9
Christian Brueffer <christian@brueffer.io>
2018-09-30 10:24:21 +0200

    Recipe cleanup.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,10 @@ export LDFLAGS="$(gsl-config --libs)"
 # For whatever reason, it can't link to gsl correctly without this on OS X.
 export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
 
-$R CMD INSTALL --build .
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  $R CMD INSTALL --build .
+else
+  mkdir -p $PREFIX/lib/R/library/gsl
+  mv * $PREFIX/lib/R/library/gsl
+fi
